23a1b736d3541dcfcc90d6d8b6fe54be73004186
Rafael P Monteiro <rafael.p.monteiro@gmail.com>
2020-09-23 16:41:44 -0300

    Initial version
    This little application is to assist lack of library for this in the Java language.
    I use to get the processor id.
    I hope this app helps you.

---
+++ GetInfoPc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace GetSerialNumber
+{
+    class Program{
+
+        static void Main(string[] args)
+        {
+            string cpuInfo = string.Empty;
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                foreach(PropertyData pdc in mo.Properties)
+                {
+                    Console.WriteLine(pdc.Name+": "+pdc.Value);
+                }
+                //cpuInfo = mo.Properties["processorID"].Value.ToString();
+                break;
+            }
+        }
+    }
+}
---
+++ GetInfoPc/Test.bat
@@ -0,0 +1,3 @@
+bin\Release\GetInfoPc.exe
+
+@pause
